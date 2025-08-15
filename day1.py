71ca86a59733c1a0e435e29dfc9118975b92d475
Patrick Mejia <patrick.mejia@aol.com>
2025-08-15 11:31:30 -0400

    Update day1.py

--- day1.py
+++ day1.py
@@ -2,3 +2,10 @@
 
 evens = [n for n in range(1,21) if n%2 == 0]
 print(evens)
+
+# Given a list of words return a dict of word → length
+
+sports = ["Soccer", "Golf", "Basketball", "Volleyball", "Baseball"]
+word_lengths = {sport: len(sport) for sport in sports}
+
+print(f"\n{word_lengths}\n")
