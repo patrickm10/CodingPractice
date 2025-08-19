f78fd52c86b0ed7e6916cc7534179c2d893d2c21
Patrick Mejia <patrick.mejia@aol.com>
2025-08-19 13:48:54 -0400

    Update day3.sql

--- day3.sql
+++ day3.sql
@@ -18,3 +18,28 @@ SELECT
 FROM Products
 WHERE low_fats = 'Y'
 AND recyclable = 'Y'
+
+
+-- Find the names of the customer that are either:
+-- referred by any customer with id != 2.
+-- not referred by any customer.
+
+SELECT name
+FROM Customer
+WHERE  referee_id !=2
+OR referee_id IS NULL
+
+-- A country is big if:
+-- it has an area of at least three million (i.e., 3000000 km2), or
+-- it has a population of at least twenty-five million (i.e., 25000000).
+-- Write a solution to find the name, population, and area of the big countries.
+
+SELECT name,
+    population,
+    area
+FROM World
+WHERE population >= 25000000
+OR area >= 3000000
+
+
+--
