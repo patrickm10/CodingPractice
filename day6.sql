e92730a9b1e61b4e1702c0d5f82a6f2996ab7b8d
Patrick Mejia <patrick.mejia@aol.com>
2025-08-22 16:40:10 -0400

    Update day6.sql

--- day6.sql
+++ day6.sql
@@ -6,3 +6,18 @@ SELECT
     budget
 FROM department
 WHERE budget > 1000000
+
+
+
+-- Find the average salary per department.
+
+SELECT
+    d.dept_name,
+    ROUND(AVG(e.salary),2) AS avg_salary
+FROM employee e
+JOIN department d ON e.dept_id=d.dept_id
+GROUP BY d.dept_name
+
+
+
+-- Return employees whose salary is above their department’s average.
