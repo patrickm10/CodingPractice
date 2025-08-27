8da4425e2b9c989d981d2b29cbaf278068cd9e1b
Patrick Mejia <patrick.mejia@aol.com>
2025-08-27 12:17:37 -0400

    Update day9.sql

--- day9.sql
+++ day9.sql
@@ -28,3 +28,46 @@ SELECT dept_name,
 DROP view high_earners
 
 
+
+
+-- For each dept compute headcount and average yoe
+
+SELECT 
+    d.dept_id,
+    d.dept_name,
+    COUNT(*) AS headcount,
+    ROUND(AVG(e.yoe),2) AS average_yoe
+FROM employee e
+JOIN department d ON e.dept_id=d.dept_id
+GROUP BY d.dept_id, d.dept_name
+
+
+
+-- Employees hired before 2020 with dept_name
+
+SELECT 
+    e.id,
+    e.name,
+    e.hire_date,
+    d.dept_name
+FROM employee e
+JOIN department d ON e.dept_id=d.dept_id
+WHERE EXTRACT(YEAR from e.hire_date) > 2020
+
+
+
+-- List depts where every employee earns more than 100,000
+
+SELECT 
+    d.dept_id,
+    d.dept_name,
+    COUNT(*) AS headcount,
+    AVG(e.salary) AS average_salary
+FROM employee e
+JOIN department d ON e.dept_id=d.dept_id
+GROUP BY d.dept_id, d.dept_name
+HAVING MIN(e.salary) > 100000
+
+
+
+
