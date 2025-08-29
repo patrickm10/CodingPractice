e1894ae61cf297f01107c6f784c7a0b6749bd99c
Patrick Mejia <patrick.mejia@aol.com>
2025-08-29 13:01:10 -0400

    Update day12.py

--- day12.py
+++ day12.py
@@ -39,3 +39,33 @@ else:
             print(f"Not valid {n}")
 
 print(f"{result}\n")
+
+
+# Read a text file and count the number of lines
+file_path = "shakespeare.txt"
+
+def read_text_file(file_path):
+    with open(file_path, "r") as f:
+        return sum(1 for _ in f)
+
+print(f"Number of lines: {read_text_file(file_path)}\n")
+
+
+
+# Count how many times each word appears
+
+def word_count(file_path, stopwords=None):
+    if stopwords is None:
+        stopwords = {"the", "and", "a", "is", "of", "but","for", "thee"}  # default ignored words
+    with open(file_path, "r") as f:
+        text = f.read().lower().replace(".", "")
+        words = text.replace(",", "").split()
+        filtered_words = [w for w in words if w not in stopwords]
+        return Counter(filtered_words)
+
+counts = word_count(file_path, stopwords={"the", "and", "to", "in", "of", "my", "thy", "thou", "i", "with", "that", "but","for", "thee", "a"})
+# for word, freq in counts.items():
+#     print(f"{word}: {freq}")
+
+print(counts.most_common(3))
+
